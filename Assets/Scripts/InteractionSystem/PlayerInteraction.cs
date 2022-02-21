11babb73fc59b0440d5ea80f52682234f4c2c430
Awesome Mike <magicmicheal4@gmail.com>
2022-02-21 18:37:45 +0100

    Interaction Prototype

---
+++ Assets/Scripts/InteractionSystem/PlayerInteraction.cs
@@ -0,0 +1,102 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public float interactionDistance;
+    public bool enableRaycast;
+    public TextMeshProUGUI interactionText;
+    public GameObject interactionClickGO;
+    public GameObject interactionHoldGO; // the ui parent to disable when not interacting
+    public Image interactionHoldProgress; // the progress bar for hold interaction type
+
+    Camera cam;
+    public bool successfulHit = false;
+    Interactable i;
+    KeyCode key = KeyCode.E;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*        if (enableRaycast)
+                {
+                    Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+                    RaycastHit hit;
+
+
+
+                    if (Physics.Raycast(ray, out hit, interactionDistance))
+                    {
+                        Interactable interactable = hit.collider.GetComponent<Interactable>();
+                        successfulHit = UpdateUI(successfulHit, interactable);
+                    }
+                }*/
+
+        if (successfulHit && Input.GetKey(key))
+            HandleInteraction(i);
+    }
+
+    public void UpdateUI(bool _successfulHit, Interactable interactable)
+    {
+        successfulHit = _successfulHit;
+        if (_successfulHit)
+        {
+            i = interactable;
+            interactionText.text = interactable.GetDescription();
+            //successfulHit = true;
+            //Debug.Log("E");
+            interactionClickGO.SetActive(interactable.interactionType == Interactable.InteractionType.Click);
+            interactionHoldGO.SetActive(interactable.interactionType == Interactable.InteractionType.Hold);
+        }
+        else if (!_successfulHit)
+        {
+            interactionText.text = "";
+            interactionHoldGO.SetActive(false);
+            interactionClickGO.SetActive(false);
+        }
+    }
+
+    void HandleInteraction(Interactable interactable)
+    {
+
+        switch (interactable.interactionType)
+        {
+            case Interactable.InteractionType.Click:
+                // interaction type is click and we clicked the button -> interact
+                if (Input.GetKeyDown(key))
+                {
+                    interactable.Interact();
+                }
+                break;
+            case Interactable.InteractionType.Hold:
+                if (Input.GetKey(key))
+                {
+                    // we are holding the key, increase the timer until we reach 1f
+                    interactable.IncreaseHoldTime();
+                    if (interactable.GetHoldTime() > 1f)
+                    {
+                        successfulHit = false;
+                        interactable.Interact();
+                        interactable.ResetHoldTime();
+                    }
+
+                }
+                else
+                {
+                    interactable.ResetHoldTime();
+                }
+                interactionHoldProgress.fillAmount = interactable.GetHoldTime();
+                break;
+
+            // helpful error for us in the future
+            default:
+                throw new System.Exception("Unsupported type of interactable.");
+        }
+    }
+}
