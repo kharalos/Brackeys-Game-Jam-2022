a749484145e732c1ae19833c0bc3e76195267803
Nik <nnevermann@gmail.com>
2022-02-23 14:23:43 +0100

    added models to test scene

---
+++ Assets/Scripts/DebugUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DebugUI : MonoBehaviour
+{
+
+    int fps;
+    int verts;
+
+    private void Start() {
+        StartCoroutine(MyMethod());
+    }
+
+    IEnumerator MyMethod() {
+        while (true) {
+            fps = (int)(1f / Time.unscaledDeltaTime);
+            #if UNITY_EDITOR
+            verts = UnityStats.vertices/1000;
+            #endif
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void OnGUI() {
+        GUI.Label(new Rect(10, 10, 100, 20), $"fps: {fps}");
+        #if UNITY_EDITOR
+        GUI.Label(new Rect(10, 30, 100, 20), $"verts: {verts}k");
+        #endif
+    }
+}
